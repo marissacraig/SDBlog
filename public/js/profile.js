822b1806b54508b29d14595dea15aa1bae05a17f
marissacraig <gersen.marissa@gmail.com>
2023-09-22 06:12:37 -0700

    fixed bug in js files

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,23 +1,22 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const name = document.querySelector('#blog-name').value.trim();
+  const body = document.querySelector('#blog-body').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (name && body) {
+    const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, body }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create blog post');
     }
   }
 };
