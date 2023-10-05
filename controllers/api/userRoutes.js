b77805b3a555990f21e971f87740690d1ec65e83
marissacraig <gersen.marissa@gmail.com>
2023-10-04 18:01:52 -0700

    separated login and sign up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get("/", async (req, res) => { 
+  try { 
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+    });
+    res.status(200).json(userData);
+  } catch (err) {res.status(500).json(err) }
+});
+
+router.get("/:id", async (req, res) => { 
+  
+});
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
